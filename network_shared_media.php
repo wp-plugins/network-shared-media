d12b5847b2e0ca63af204f77320a5ada8c92286f
joostdekeijzer <joostdekeijzer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-25 10:11:05 +0000

    Set correct is_admin() value (using IS_ADMIN constant) fixing Contact Form 7 issue (and possibly others)
    git-svn-id: https://plugins.svn.wordpress.org/network-shared-media/tags/0.9.3@577067 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- network_shared_media.php
+++ network_shared_media.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Netword_Shared_Media
- * @version 0.9.1
+ * @version 0.9.3
  */
 /*
 Plugin Name: Network Shared Media
@@ -9,7 +9,7 @@ Plugin URI: http://wordpress.org/extend/plugins/network-shared-media/
 Description: This plugin adds a new tab to the "Add Media" window, allowing you to access media in other sites. Based on an idea of Aaron Eaton
 Author: Joost de Keijzer
 Author URI: http://dekeijzer.org/
-Version: 0.9.1
+Version: 0.9.3
 Licence: GPLv2 or later
 */
 
@@ -91,6 +91,9 @@ class network_shared_media {
 			return;
 		}
 
+		// set the first part of the form action url now, to the current active site, to prevent X-Frame-Options problems
+		$form_action_url = plugins_url( 'media-upload.php', __FILE__ );
+
 		$nsm_blog_id = null;
 		if( !array_key_exists( 'blog_id', $_GET ) ) $_GET['blog_id'] = null;
 
@@ -113,7 +116,7 @@ class network_shared_media {
 		// fix to make get_media_item add "Insert" button
 		unset($_GET['post_id']);
 
-		$form_action_url = plugins_url( 'media-upload.php', __FILE__ ) . "?type=$type&tab=library&post_id=$post_id&blog_id=$blog_id";
+		$form_action_url .= "?type=$type&tab=library&post_id=$post_id&blog_id=$blog_id";
 		$form_action_url = apply_filters('media_upload_form_url', $form_action_url, $type);
 
 		$form_class = 'media-upload-form validate';
